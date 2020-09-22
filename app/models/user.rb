b8065060027b1103c7b6ee2ae8a5d501027742c8
Grace Teng <grace@pelicularities.net>
2020-09-22 12:41:29 +0800

    added new fields to user, updated ApplicationController to allow new fields, and added assocs / validations to user

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,8 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  
+  has_many :user_meals
+  has_many :orders
+  validates :first_name, :last_name, :protein, :carbs, :fats, presence: true
 end
