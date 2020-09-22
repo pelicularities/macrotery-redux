f6db9b133b17d9b8da5408d07d59138537195ec8
Grace Teng <grace@pelicularities.net>
2020-09-22 11:47:33 +0800

    fixed create_dishes error, added some validations to cuisine, dish and eatery

--- app/models/dish.rb
+++ app/models/dish.rb
@@ -1,3 +1,5 @@
 class Dish < ApplicationRecord
   belongs_to :eatery
+  validates :name, :price, :protein, :carbs, :fats, presence: true
+  validates :name, uniqueness: { scope: :eatery, message: 'eatery already has a dish by this name' }
 end
--- app/models/eatery.rb
+++ app/models/eatery.rb
@@ -1,2 +1,5 @@
 class Eatery < ApplicationRecord
+  validates :name, :address, :phone_number, presence: true, uniqueness: true
+  has_many :eatery_cuisines
+  has_many :cuisines, through: :eatery_cuisines
 end
