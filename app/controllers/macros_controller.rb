8321c4c5bd0c8683e110f560291b0731f88084ab
allen chung <east11@gmail.com>
2020-09-24 18:25:19 +0800

    macros finally done

--- app/controllers/macros_controller.rb
+++ app/controllers/macros_controller.rb
@@ -1,51 +1,59 @@
 class MacrosController < ApplicationController
-  # before_action :set_macros
+before_action :set_macros, only: [:edit, :show, :update, :destroy]
 
+  def index
+    @macro= current_user.macros.order(updated_at: :desc)
+  end
 
-def index
-  @macro= Macro.all
-end
-
-def new
-  @macro = Macro.new
-end
-
-def create
-  macro= Macro.new(macro_params)
-  macro.user = current_user
-  if macro.save!
-    redirect_to macros_path
-  else
+  def new
     @macro = Macro.new
-    render :new
   end
-end
 
-def edit
-end
-
-def update
-  @macro.update(macro_params)
-  if @macro.save
-    redirect_to profile_macros_path(@macro)
-  else
-    render :edit
+  def create
+    macro= Macro.new(macro_params)
+    macro.user = current_user
+    if macro.save!
+      redirect_to macros_path
+    else
+      @macro = Macro.new
+      render :new
+    end
   end
-end
 
-def destroy
-  @macro.destroy
-  redirect_to macros_path
-end
+  # def show
+  # end
+
+  def edit
+  end
+
+  def update
+    @macro.update(macro_params)
+    if @macro.save
+      redirect_to macros_path(@macro)
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @macro.destroy
+    redirect_to macros_path
+  end
+
 
 private
 
-# def set_macros
-#   @macro= Macro.find(params[:id])
-# end
 
-def macro_params
-  params.require(:macro).permit(:name, :protein, :carbs, :fats)
-end
+  def set_macros
+    @macro= Macro.find(params[:id])
+  end
+
+  def macro_params
+    params.require(:macro).permit(:name, :protein, :carbs, :fats)
+  end
+
+  def most_recent
+  end
+
 
 end
--- app/models/macro.rb
+++ app/models/macro.rb
@@ -2,7 +2,12 @@ class Macro < ApplicationRecord
   belongs_to :user
   validates :name, :protein, :carbs, :fats, presence: true
   validates :name, uniqueness: { scope: :user, message: 'User already has a meal by this name' }
+
+  validates :protein, :carbs, :fats, numericality: {only_integer: true, greater_than_or_equal_to: 1, less_than_or_equal_to: 200}
+
+
   after_find :calculate_calories, if: :calories_empty?
+
 
   private
 
