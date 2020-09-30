a4d737e7e11068110c58cf3e2f748c1ad698fa16
Zack Xu <zack.xu@hotmail.com>
2020-09-30 14:54:48 +0800

    eatery show UI

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -63,7 +63,7 @@ Rails.application.configure do
 
   # ngrok hosts
   config.hosts << "4c83dfd1de83.ngrok.io"
-  config.hosts << "c3e2cb22fcc2.ngrok.io" # Zack's ngrok
+  config.hosts << "e005bd3f3c28.ngrok.io" # Zack's ngrok
   config.hosts << "4993bae4096c.ngrok.io"
   config.hosts << "87f21c268b36.ngrok.io"
 
