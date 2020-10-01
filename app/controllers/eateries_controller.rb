ec614a39aaae519f46b3ea225485e14cb07029bc
Grace Teng <grace@pelicularities.net>
2020-10-01 19:56:32 +0800

    order page now warns you if you exceed your macros. kind of

--- app/controllers/eateries_controller.rb
+++ app/controllers/eateries_controller.rb
@@ -3,5 +3,8 @@ class EateriesController < ApplicationController
   def show
     @eatery = Eatery.find(params[:id])
     @order = Order.new
+    @protein = params[:protein]
+    @carbs = params[:carbs]
+    @fats = params[:fats]
   end
 end
