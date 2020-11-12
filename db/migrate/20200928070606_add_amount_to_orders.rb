f855799b6e3f33cefd8c750c770adb1e719615ee
Grace Teng <grace@pelicularities.net>
2020-11-12 20:55:53 +0800

    migrations are out of sync -- pending migrations are not actually pending. commented out the un-run migrations

--- db/migrate/20200928070606_add_amount_to_orders.rb
+++ db/migrate/20200928070606_add_amount_to_orders.rb
@@ -1,5 +1,5 @@
-class AddAmountToOrders < ActiveRecord::Migration[6.0]
-  def change
-     add_monetize :orders, :amount, currency: { present: false }
-  end
-end
+# class AddAmountToOrders < ActiveRecord::Migration[6.0]
+#   def change
+#      add_monetize :orders, :amount, currency: { present: false }
+#   end
+# end
--- db/migrate/20200928083228_add_session_to_order.rb
+++ db/migrate/20200928083228_add_session_to_order.rb
@@ -1,6 +1,6 @@
-class AddSessionToOrder < ActiveRecord::Migration[6.0]
-  def change
-    add_column :orders, :checkout_session_id, :string
-    add_column :orders, :state, :string, default: "pending"
-  end
-end
+# class AddSessionToOrder < ActiveRecord::Migration[6.0]
+#   def change
+#     add_column :orders, :checkout_session_id, :string
+#     add_column :orders, :state, :string, default: "pending"
+#   end
+# end
